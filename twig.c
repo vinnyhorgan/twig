1248c56c218f237d1c6c64df1def1b81b6465db3
Daniel Vincent Horgan <vinnyhorgan@gmail.com>
2025-07-25 19:22:52 +0200

    set up window

--- twig.c
+++ twig.c
@@ -14,8 +14,14 @@
 
 #include <miniz.h>
 
-static SDL_Window* window = NULL;
-static SDL_Renderer* renderer = NULL;
+#define WIDTH 320
+#define HEIGHT 240
+
+typedef struct {
+  SDL_Window* window;
+  SDL_Renderer* renderer;
+  SDL_Texture* texture;
+} State;
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
   SDL_SetAppMetadata("twig", "0.1", "com.vinnyhorgan.twig");
@@ -25,10 +31,43 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
     return SDL_APP_FAILURE;
   }
 
-  if (!SDL_CreateWindowAndRenderer("twig", 640, 480, 0, &window, &renderer)) {
+  State* state = (State*)SDL_calloc(1, sizeof(State));
+  if (!state) {
+    SDL_Log("failed to allocate state: %s", SDL_GetError());
+    return SDL_APP_FAILURE;
+  }
+
+  *(State**)appstate = state;
+
+  SDL_EnableScreenSaver();
+  SDL_SetHint(SDL_HINT_MOUSE_FOCUS_CLICKTHROUGH, "1");
+
+  float scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
+
+  if (!SDL_CreateWindowAndRenderer("twig", 640 * scale, 480 * scale, SDL_WINDOW_RESIZABLE, &state->window,
+                                   &state->renderer)) {
     SDL_Log("failed to create window/renderer: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
+
+  SDL_SetRenderVSync(state->renderer, 1);
+  SDL_SetRenderLogicalPresentation(state->renderer, WIDTH, HEIGHT, SDL_LOGICAL_PRESENTATION_INTEGER_SCALE);
+
+  SDL_PropertiesID props = SDL_CreateProperties();
+  SDL_SetNumberProperty(props, SDL_PROP_TEXTURE_CREATE_ACCESS_NUMBER, SDL_TEXTUREACCESS_STREAMING);
+  SDL_SetNumberProperty(props, SDL_PROP_TEXTURE_CREATE_WIDTH_NUMBER, WIDTH);
+  SDL_SetNumberProperty(props, SDL_PROP_TEXTURE_CREATE_HEIGHT_NUMBER, HEIGHT);
+
+  state->texture = SDL_CreateTextureWithProperties(state->renderer, props);
+  if (!state->texture) {
+    SDL_Log("failed to create texture: %s", SDL_GetError());
+    return SDL_APP_FAILURE;
+  }
+
+  SDL_DestroyProperties(props);
+  SDL_SetTextureScaleMode(state->texture, SDL_SCALEMODE_NEAREST);
+
+  SDL_Log("%s", SDL_GetPixelFormatName(state->texture->format));
 
   lua_State* L = luaL_newstate();
   luaL_openlibs(L);
@@ -55,7 +94,38 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
 }
 
 SDL_AppResult SDL_AppIterate(void* appstate) {
+  State* state = (State*)appstate;
+
+  const double now = ((double)SDL_GetTicks()) / 1000.0;
+  const float red = (float)(0.5 + 0.5 * SDL_sin(now));
+  const float green = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
+  const float blue = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
+
+  SDL_Surface* s = NULL;
+  if (!SDL_LockTextureToSurface(state->texture, NULL, &s)) {
+    SDL_Log("failed to lock texture: %s", SDL_GetError());
+    return SDL_APP_FAILURE;
+  }
+
+  SDL_ClearSurface(s, 1, 0, 0, 1);
+  SDL_Rect rect = { 10, 10, 50, 50 };
+  SDL_FillSurfaceRect(s, &rect, SDL_MapRGB(SDL_GetPixelFormatDetails(s->format), NULL, 255, 255, 0));
+  SDL_UnlockTexture(state->texture);
+
+  SDL_SetRenderDrawColorFloat(state->renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT); /* new color, full alpha. */
+  SDL_RenderClear(state->renderer);
+  SDL_RenderTexture(state->renderer, state->texture, NULL, NULL);
+  SDL_RenderPresent(state->renderer);
+
   return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void* appstate, SDL_AppResult result) {}
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
+  if (appstate) {
+    State* state = (State*)appstate;
+    SDL_DestroyTexture(state->texture);
+    SDL_DestroyRenderer(state->renderer);
+    SDL_DestroyWindow(state->window);
+    SDL_free(state);
+  }
+}
